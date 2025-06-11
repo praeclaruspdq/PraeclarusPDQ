9fd80990d16b8fd5bbdce1f0b1b5f94d6b2a200b
praeclaruspdq <117790473+praeclaruspdq@users.noreply.github.com>
2025-06-11 13:00:28 +1000

    Update DirectlyFollows.java

--- src/main/java/com/processdataquality/praeclarus/action/DirectlyFollows.java
+++ src/main/java/com/processdataquality/praeclarus/action/DirectlyFollows.java
@@ -41,7 +41,7 @@ import org.springframework.expression.spel.ast.Selection;
  * @author Sareh Sadeghianasl
  * @date 10/2/2023
  */
-@Plugin(name = "Directly Follows", author = "Root", version = "1.0", synopsis = "Applies direclty follows operator to a log")
+@Plugin(name = "Directly Follows", author = "Sareh Sadeghianasl", version = "1.0", synopsis = "Applies direclty follows operator to a log")
 public class DirectlyFollows extends AbstractAction {
 
 	public DirectlyFollows() {
