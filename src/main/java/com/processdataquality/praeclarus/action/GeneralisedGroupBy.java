a08bd4f396a7ff2bad1a44b5097e94bbd6ee580a
praeclaruspdq <117790473+praeclaruspdq@users.noreply.github.com>
2025-06-11 12:42:20 +1000

    Update GeneralisedGroupBy.java

--- src/main/java/com/processdataquality/praeclarus/action/GeneralisedGroupBy.java
+++ src/main/java/com/processdataquality/praeclarus/action/GeneralisedGroupBy.java
@@ -41,7 +41,7 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.columns.Column;
 
 /**
- * @author root
+ * @author Sareh Sadeghianasl
  * @date 10/2/2023
  */
 @Plugin(name = "Generalised Group-By", author = "Root", version = "1.0", synopsis = "Groups rows based on given conditions")
